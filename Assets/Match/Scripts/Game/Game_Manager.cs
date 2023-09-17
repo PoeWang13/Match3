768fb6bdf856c6530dccb5096b5335abfd09b170
PoeWang13 <53164541+PoeWang13@users.noreply.github.com>
2023-09-17 15:00:27 +0300

    Add summer
    Add some summer for boardSize and boardPosition

--- Assets/Match/Scripts/Game/Game_Manager.cs
+++ Assets/Match/Scripts/Game/Game_Manager.cs
@@ -4,9 +4,18 @@ public class Game_Manager : MonoBehaviour
 {
 	private static Game_Manager instance;
 	public static Game_Manager Instance { get => instance; }
+
+    [Header("Size of board")]
     [SerializeField] private Vector2Int boardSize;
+    [Header("The starting position where the board will be on the screen.")]
     [SerializeField] private Vector3 boardPosition;
+    /// <summary>
+    /// Size of board
+    /// </summary>
     public Vector2Int BoardSize { get => boardSize; }
+    /// <summary>
+    /// The starting position where the board will be on the screen.
+    /// </summary>
     public Vector3 BoardPosition { get => boardPosition; }
     private void Awake()
     {
